86a0a1e62c632ccf8966f8e20145af1db35d81df
wuhongyun <451863397@qq.com>
2020-05-21 14:21:11 +0800

    nacos discovery服务注册发现

---
+++ nacos-customer/src/main/java/com/zxw/nacoscustomer/NacosCustomerApplication.java
@@ -0,0 +1,51 @@
+package com.zxw.nacoscustomer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosCustomerApplication.class, args);
+    }
+
+    //Instantiate RestTemplate Instance
+    @Bean
+    public RestTemplate restTemplate(){
+
+        return new RestTemplate();
+    }
+
+    @RestController
+    public class NacosController{
+
+        @Autowired
+        private LoadBalancerClient loadBalancerClient;
+        @Autowired
+        private RestTemplate restTemplate;
+
+        @Value("${spring.application.name}")
+        private String appName;
+
+        @GetMapping("/echo/app-name")
+        public String echoAppName(){
+            //Access through the combination of LoadBalanceClient and RestTemolate
+            ServiceInstance serviceInstance = loadBalancerClient.choose("nacos-privider");
+            String path = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
+            System.out.println("request path:" +path);
+            return restTemplate.getForObject(path,String.class);
+        }
+
+    }
+}
